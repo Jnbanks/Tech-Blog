eab6c3799fa4a38bf05ea4977a54c9b338dd3913
JnBanks <joku.banks@gmail.com>
2022-06-02 10:15:10 -0700

    update

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {User,Blog} = require("../models/");
-const bcrypt  = require("bcrypt");
+const bcrypt  = require("bcryptjs");
 
 //find all
 router.get("/", (req, res) => {
--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@
 
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 
 class User extends Model {}
 
